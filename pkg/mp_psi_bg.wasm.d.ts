d5f77e62def9efe7b740247a4da1d59eb443a99f
Vivek Bhupatiraju <vivekab@mit.edu>
2024-02-23 19:36:15 -0700

    add wasm pkg

---
+++ pkg/mp_psi_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function gen_keys_js(): number;
+export function round1_js(a: number, b: number, c: number, d: number): number;
+export function round2_js(a: number, b: number, c: number, d: number): number;
+export function round3_js(a: number, b: number, c: number): void;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export function __wbindgen_add_to_stack_pointer(a: number): number;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __wbindgen_exn_store(a: number): void;
